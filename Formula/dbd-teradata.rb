ee6dc7062a8e2eccdc8af1cd8b1a56ef91d356ff
Fernando Freire <dogonthehorizon@gmail.com>
2014-05-27 13:42:32 -0700

    Experimental formulae for tdsql.

---
+++ Formula/dbd-teradata.rb
@@ -0,0 +1,31 @@
+require "formula"
+
+class DbdTeradata < Formula
+  homepage "http://www.presicient.com/tdatdbd/"
+  url "http://www.presicient.com/tdatdbd/DBD-Teradata-12.001.tar.gz"
+  sha1 "e206b0b07bcb6d20d7bf32dbffe5ff8812679ca6"
+
+  depends_on 'Crypt::ECB' => :perl
+  depends_on 'Crypt::Rijndael' => :perl
+  depends_on 'Digest::SHA1' => :perl
+
+  def install
+    Dir.chdir("DBD-Teradata-12.001")
+
+    system "perl", "Makefile.PL"
+    system "make"
+    system "make", "install"
+  end
+
+  test do
+    # `test do` will create, run in and delete a temporary directory.
+    #
+    # This test will fail and we won't accept that! It's enough to just replace
+    # "false" with the main program this formula installs, but it'd be nice if you
+    # were more thorough. Run the test with `brew test DBD-Teradata`.
+    #
+    # The installed folder is not in the path, so use the entire path to any
+    # executables being tested: `system "#{bin}/program", "do", "something"`.
+    system "false"
+  end
+end
---
+++ Formula/tdsql.rb
@@ -0,0 +1,44 @@
+require "formula"
+
+# Documentation: https://github.com/Homebrew/homebrew/wiki/Formula-Cookbook
+#                /usr/local/Library/Contributions/example-formula.rb
+# PLEASE REMOVE ALL GENERATED COMMENTS BEFORE SUBMITTING YOUR PULL REQUEST!
+
+class Tdsql < Formula
+  homepage "https://github.com/groupon/tdsql"
+  url "https://github.com/groupon/tdsql/archive/tdsql-2014.05.20_18.30.tar.gz"
+  sha1 "51c765151bb4b41b5bc469960dff8e4f8739bc20"
+
+  depends_on 'readline'
+  depends_on 'cpanminus'
+  depends_on 'dbd-teradata'
+
+  depends_on 'DBI' => :perl
+  depends_on 'Term::ReadKey' => :perl
+  depends_on 'CSV_XS' => :perl
+
+  def install
+    # ENV.deparallelize  # if your formula fails when building in parallel
+
+    # So, this is generally a Bad Idea but in this case we just need
+    # to temporarily link to readline so this module builds successfully.
+    system "brew", "link", "--force", "readline"
+    system "cpanm", "Term::ReadLine::Gnu"
+    system "brew", "unlink", "readline"
+
+    bin.install "tdsql"
+    man1.install "tdsql.1"
+  end
+
+  test do
+    # `test do` will create, run in and delete a temporary directory.
+    #
+    # This test will fail and we won't accept that! It's enough to just replace
+    # "false" with the main program this formula installs, but it'd be nice if you
+    # were more thorough. Run the test with `brew test tdsql`.
+    #
+    # The installed folder is not in the path, so use the entire path to any
+    # executables being tested: `system "#{bin}/program", "do", "something"`.
+    system "false"
+  end
+end
